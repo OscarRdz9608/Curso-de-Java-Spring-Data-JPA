01b147c75d386f64e7e563d7e161281712a15b70
Oscar David Rodriguez Marin <oscar.d.rodriguez@ids.com.mx>
2023-11-10 14:40:04 -0600

    Terminado

---
+++ src/main/java/com/platzi/pizza/persitence/projection/OrderSummary.java
@@ -0,0 +1,14 @@
+package com.platzi.pizza.persitence.projection;
+
+import java.time.LocalDateTime;
+
+public interface OrderSummary {
+
+
+    Integer getIdOrder();
+    String getCustomerName();
+    LocalDateTime getOrderDate();
+    Double getOrderTotal();
+    String getPizzaNames();
+
+}

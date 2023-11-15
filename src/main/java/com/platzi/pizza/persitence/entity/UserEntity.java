3fc2eabc669bc55e3242f7b4502db8ff89aee97f
Oscar David Rodriguez Marin <oscar.d.rodriguez@ids.com.mx>
2023-11-14 22:39:08 -0600

    Curso Terminado

---
+++ src/main/java/com/platzi/pizza/persitence/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.platzi.pizza.persitence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean locked;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean disabled;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<UserRoleEntity> roles;
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", locked=" + locked +
+                ", disabled=" + disabled +
+                ", roles=" + roles +
+                '}';
+    }
+}
